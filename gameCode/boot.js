f37c12cab3023de4b7b29d6a20bf208ebba3efa9
Ilya <ilyas.sporichev@gmail.com>
2017-04-12 12:58:37 +0300

    small game\feature: menu state

--- gameCode/boot.js
+++ gameCode/boot.js
@@ -2,6 +2,7 @@ let bootState = {
 	preload: function() {
 		game.load.image('preloadBar', 'assets/bar.png');
 		game.load.image('logo', 'assets/logo.png');
+		game.load.image('menu', 'assets/menu.png')
 	},
 	create: function() {
 		game.physics.startSystem(Phaser.Physics.ARCADE);
--- gameCode/menu.js
+++ gameCode/menu.js
@@ -1,10 +1,13 @@
 let menuState = {
 	create: function() {
-		let nameLabel = game.add.text(90, 90, 'The Knight', {fill: '#fff'});
-		let startLabel = game.add.text(90, 190, 'press W to start',{fill: '#fff'});
-		let wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+		// let nameLabel = game.add.text(90, 90, 'The Knight', {fill: '#fff'});
+		// let startLabel = game.add.text(90, 190, 'press W to start',{fill: '#fff'});
+		let menu = game.add.sprite(0, 0, 'menu')
+		let spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
-		wKey.onDown.addOnce(this.start, this);
+		
+
+		spaceKey.onDown.addOnce(this.start, this);
 	},
 
 	start: function() {
